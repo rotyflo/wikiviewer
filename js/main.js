428f8bf22017dc5d6e49010f88106b6dde21b85b
RT <me@rtflores.com>
2021-09-21 23:01:08 -0700

    generate random previews within app

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $("#query").val("");  // Clear query on refresh
 
-$("#query").on("keydown", function(input) {
+$("#query").on("keydown", function (input) {
   // if 'enter' is pressed
   if (input.keyCode === 13) {
     searchWiki($("#query").val());
   }
 });
 
-$("button").on("click", function() {
+$("#submit").on("click", function () {
   searchWiki($("#query").val());
+});
+
+$("#random").on("click", function () {
+  getRandomWiki();
 });
 
 function searchWiki(query) {
@@ -34,6 +38,26 @@ function searchWiki(query) {
       `;
     }
 
-    return $("#results").html(cards);
+    $("#results").html(cards);
+  });
+}
+
+function getRandomWiki() {
+  let url = "https://en.wikipedia.org/api/rest_v1/page/random/summary";
+
+  $.getJSON(url, function (api) {
+    let title = api.title;
+    let extract = api.extract;
+    let pageid = api.pageid;
+
+    let card = `
+      <p>
+        <a target="_blank" href="https://en.wikipedia.org/?curid=${pageid}">${title}</a>
+        <br>
+        ${extract}
+      </p>
+      `;
+
+    $("#results").html(card);
   });
 }
