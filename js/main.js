ee05b348697659ae2c33f42134ef283b3046d502
RT Flores <me@rtflores.com>
2022-03-09 10:43:24 -0800

    convert spaces to tabs and tweak svg layout

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ $("#query").val("");  // Clear query on refresh
 getRandomWiki();
 
 $("#query").on("keydown", input => {
-  // if 'enter' is pressed
-  if (input.keyCode === 13) {
-    searchWiki($("#query").val());
-  }
+	// if 'enter' is pressed
+	if (input.keyCode === 13) {
+		searchWiki($("#query").val());
+	}
 });
 
 $("#submit").on("click", () => searchWiki( $("#query").val() ));
@@ -13,48 +13,48 @@ $("#random").on("click", () => getRandomWiki());
 $("#clear").on("click", () => $("#query").val(""));
 
 function searchWiki(query) {
-  let url = `https://en.wikipedia.org/w/api.php?callback=?&action=query&format=json&explaintext&prop=extracts&generator=search&exsentences=1&exintro=1&gsrsearch=${query}`;
+	let url = `https://en.wikipedia.org/w/api.php?callback=?&action=query&format=json&explaintext&prop=extracts&generator=search&exsentences=1&exintro=1&gsrsearch=${query}`;
 
-  $.getJSON(url, api => {
-    let pages = api.query.pages;
-    let keys = Object.keys(pages);
-    let numberOfResults = Object.keys(api.query.pages).length;
-    let cards = "";
+	$.getJSON(url, api => {
+		let pages = api.query.pages;
+		let keys = Object.keys(pages);
+		let numberOfResults = Object.keys(api.query.pages).length;
+		let cards = "";
 
-    for (let i = 0; i < numberOfResults; i++) {
-      let title = pages[keys[i]].title;
-      let extract = pages[keys[i]].extract;
-      let pageid = keys[i];
+		for (let i = 0; i < numberOfResults; i++) {
+			let title = pages[keys[i]].title;
+			let extract = pages[keys[i]].extract;
+			let pageid = keys[i];
 
-      cards += `
-      <p>
-        <a target="_blank" href="https://en.wikipedia.org/?curid=${pageid}">${title}</a>
-        <br>
-        ${extract}
-      </p>
-      `;
-    }
+			cards += `
+			<p>
+				<a target="_blank" href="https://en.wikipedia.org/?curid=${pageid}">${title}</a>
+				<br>
+				${extract}
+			</p>
+			`;
+		}
 
-    $("#results").html(cards);
-  });
+		$("#results").html(cards);
+	});
 }
 
 function getRandomWiki() {
-  let url = "https://en.wikipedia.org/api/rest_v1/page/random/summary";
+	let url = "https://en.wikipedia.org/api/rest_v1/page/random/summary";
 
-  $.getJSON(url, api => {
-    let title = api.title;
-    let extract = api.extract;
-    let pageid = api.pageid;
+	$.getJSON(url, api => {
+		let title = api.title;
+		let extract = api.extract;
+		let pageid = api.pageid;
 
-    let card = `
-      <p>
-        <a target="_blank" href="https://en.wikipedia.org/?curid=${pageid}">${title}</a>
-        <br>
-        ${extract}
-      </p>
-      `;
+		let card = `
+			<p>
+				<a target="_blank" href="https://en.wikipedia.org/?curid=${pageid}">${title}</a>
+				<br>
+				${extract}
+			</p>
+			`;
 
-    $("#results").html(card);
-  });
+		$("#results").html(card);
+	});
 }
